8bbd701df59803f205ac9879cc8a66df9bf4e5ef
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-06-12 02:49:35 +1000

    adds nullable fix for endDate

--- src/main/java/io/mac/employeetracker/config/ModelMapperConfig.java
+++ src/main/java/io/mac/employeetracker/config/ModelMapperConfig.java
@@ -12,7 +12,7 @@ public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setSkipNullEnabled(true);
+        modelMapper.getConfiguration().setSkipNullEnabled(false);
         modelMapper.typeMap(String.class, String.class).setConverter(new StringTrimConverter());
         return modelMapper;
     }
